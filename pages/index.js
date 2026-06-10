f26a2309d954dff8a69e6ff9111fc6009656c87f
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-06-10 16:01:18 +0200

    installing swr and fetching data with error handling

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,12 @@
+import useSWR from "swr";
+
 export default function HomePage() {
+  const { data: entries, isLoading, error } = useSWR("/api/Entry");
+  if (isLoading) return <p>sorting your activities…</p>;
+
+  if (!entries || error) {
+    return <h1>Oops… something went wrong.</h1>;
+  }
   return (
     <div>
       <h1>Hello from Next.js</h1>
