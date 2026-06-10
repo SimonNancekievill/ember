d59b0a0970ba0e6339392c681363291e8139bc5b
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-06-10 17:30:50 +0200

    added some styling to the list and items

--- components/ActivityList/index.js
+++ components/ActivityList/index.js
@@ -1,14 +1,22 @@
 import ActivityListItem from "../ActivityListItem";
+import styled from "styled-components";
 
 export default function ActivityList({ entries }) {
   return (
-    <ul>
+    <StyledList>
       {entries.map((entry) => {
         const { activities } = entry;
         return activities.map((activity) => {
           return <ActivityListItem activity={activity} key={activity.name} />;
         });
       })}
-    </ul>
+    </StyledList>
   );
 }
+
+const StyledList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  padding: 24px 48px;
+  gap: 24px;
+`;
--- components/ActivityListItem/index.js
+++ components/ActivityListItem/index.js
@@ -1,8 +1,25 @@
+import styled from "styled-components";
+import Image from "next/image";
+
 export default function ActivityListItem({ activity }) {
   return (
-    <li key={activity.name}>
+    <StyledItem key={activity.name}>
       <h2>{activity.name}</h2>
-      <p>{activity.category}</p>
-    </li>
+      <section>
+        <p>{activity.category}</p>
+      </section>
+    </StyledItem>
   );
 }
+
+const StyledItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  height: 160px;
+  gap: 16px;
+  border-radius: 8px;
+  padding: 8px 32px;
+  background-color: #fff;
+`;
