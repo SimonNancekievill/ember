6f1ccbc2aa6e7baee12ce0a398c196bfad521121
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-06-11 12:07:06 +0200

    removing button and form

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import ActivityList from "@/components/ActivityList";
 import useSWR from "swr";
-import Button from "@/components/Button";
 import styled from "styled-components";
-import Form from "@/components/Form";
 
 export default function HomePage() {
   const { data: entries, isLoading, error } = useSWR("/api/entries");
@@ -16,19 +14,7 @@ export default function HomePage() {
   return (
     <>
       <h1>Good to see you!</h1>
-      <ButtonWrapper>
-        <Button type="button" aria-label="Open Activity Form">
-          Add new
-        </Button>
-      </ButtonWrapper>
-      <Form />
       <ActivityList entries={entries} />
     </>
   );
 }
-
-const ButtonWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 24px 48px 0px 48px;
-`;
